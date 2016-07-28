c0063a6d713fcd8bac6eabd69ecb292c2d94aad8
Makarand <mparigi99@gmail.com>
2016-07-28 05:54:25 +0000

    set up server logic

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require("express");
+var moment = require("moment");
+
 var app = express();
 
-app.get();
+app.get("*", function (req, res) {
+    var input = req.path.toString().slice(1);
+    var output = {
+        "unix": null,
+        "natural": null
+    }
+    if () { //unix timestamp
+        console.log("passed in unix timestamp");
+        output["unix"] = ;//set outputs
+        output["natural"] = ;//set outputs 
+    } else if () { //natural language
+        console.log("passed in natural language");
+        output["unix"] = ;//set outputs
+        output["natural"] = ;//set outputs
+    }
+    
+    res.send(JSON.stringify(output));
+});
 
 app.listen(8080);
